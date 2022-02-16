1c8e7148ee8f620f52fd2fc866cf8bd4c826332c
adamc1477 <99250867+adamc1477@users.noreply.github.com>
2022-02-15 22:47:06 -0500

    Add files via upload
    Module 3 Pizza Order HW Assignment

---
+++ P1HW2_AdamCatherine.py
@@ -0,0 +1,20 @@
+# Module 3 Pizza Order HW Assignment
+# 2.15.2022
+# CTI-110 P1HW2 - Pizza Order
+# Catherine Adam
+#
+
+pizza_order = int(input('How many students do you want to order pizza for? '))
+print()
+
+print('----Pizza Order--------')
+
+print('Number of Students:', pizza_order)
+
+pizza_slices = pizza_order * 3
+print('Pizza Slices Needed:', pizza_slices)
+
+needed_pizzas = pizza_order / 2
+print('Pizzas Needed:', needed_pizzas)
+
+print('-----------------------')
