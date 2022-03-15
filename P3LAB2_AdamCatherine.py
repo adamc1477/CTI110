cac3decacf3e903b839229f60011b5bf7b31d0c7
adamc1477 <99250867+adamc1477@users.noreply.github.com>
2022-03-15 19:41:53 -0400

    Add files via upload
    Module 5 Zybooks 10.20 Lab Automobile Service Cost Assignment

---
+++ P3LAB2_AdamCatherine.py
@@ -0,0 +1,16 @@
+
+auto_service = input('Enter desired auto service:\n')
+
+print('You entered:', auto_service)
+
+if auto_service == 'Oil change':
+    print('Cost of oil change: $35')
+
+elif auto_service == 'Tire rotation':
+    print('Cost of tire rotation: $19')
+    
+elif auto_service == 'Car wash':
+    print('Cost of car wash: $7')
+    
+else:
+    print('Error: Requested service is not recognized')
