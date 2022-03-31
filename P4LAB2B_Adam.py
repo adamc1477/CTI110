e306f66282437bc84ac7de23d3270e50d48170b3
adamc1477 <99250867+adamc1477@users.noreply.github.com>
2022-03-31 01:11:16 -0400

    Add files via upload
    Module 6 Part 4 Lab 2 B Assignment, Turtle - Drawing Initials

---
+++ P4LAB2B_Adam.py
@@ -0,0 +1,31 @@
+#P4LAB2b Assignment
+#CTI-110
+#Catherine Adam
+#3/30/2022
+#
+#Using turtle graphics write a program that displays your first and last initials
+#Use color and pensize of choice.
+
+
+
+import turtle as a
+
+a.pensize(15)
+a.color("red")
+a.circle(97,-189)
+
+a.penup()
+a.right(170)
+a.forward(120)
+a.pendown()
+
+a.color("blue")
+a.right(60)
+a.forward(200)
+a.backward(200)
+a.left(-60)
+a.forward(200)
+a.backward(85)
+a.left(120)
+a.forward(105)
+
